c6f815cc75f034eb0092f3e0f37c4fd05229d04d
chrisfinne <chris.finne@gmail.com>
2019-01-19 06:16:05 -0800

    Fix for Rails 5.2 support

--- lib/postgres_ext/active_record.rb
+++ lib/postgres_ext/active_record.rb
@@ -9,7 +9,11 @@ ar_version_cutoff = gdep.matching_specs.sort_by(&:version).last
 if ar_version_cutoff
   require 'postgres_ext/active_record/5.0/query_methods'
   require 'postgres_ext/active_record/5.0/querying'
-  require 'postgres_ext/active_record/5.0/relation'
+  if Gem::Dependency.new('activerecord', '>= 5.2.0').matching_specs.sort_by(&:version).last
+    require 'postgres_ext/active_record/5.2/relation'
+  else
+    require 'postgres_ext/active_record/5.0/relation'
+  end
 else
   require 'postgres_ext/active_record/4.x/query_methods'
   require 'postgres_ext/active_record/4.x/querying'
